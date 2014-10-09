c6e610fe51bae13575be1710fea1e2d8f7822c12
William Yanez <wyanez@gmail.com>
2014-10-09 01:59:41 -0430

    hello.py: Flask basic app with templates and Web Forms using flask-wtf

--- hello.py
+++ hello.py
@@ -4,15 +4,32 @@ from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from datetime import datetime
 
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
 app = Flask(__name__)
+# Flask-WTF configuration
+app.config['SECRET_KEY'] = 'hard to guess string'
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+
+@app.route('/', methods=['GET','POST'])
 def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = '' 
     return render_template('index.html',
-                            current_time=datetime.utcnow())
+                            form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
