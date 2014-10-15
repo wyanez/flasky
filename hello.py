c1c1be42e727beee4149dc1ba21f00e2c4405c2c
William Yanez <wyanez@gmail.com>
2014-10-15 01:37:29 -0430

    Add database support wiyh SQLAlchemy. Created models User and Role

--- hello.py
+++ hello.py
@@ -7,6 +7,9 @@ from datetime import datetime
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
+
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
@@ -16,10 +19,37 @@ app = Flask(__name__)
 # Flask-WTF configuration
 app.config['SECRET_KEY'] = 'hard to guess string'
 
+#Configuration BD with SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True 
+db = SQLAlchemy(app)
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+#Model definition with SQLAlchemy
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r >' % self.username
+
+
+#App routes
 
 @app.route('/', methods=['GET','POST'])
 def index():
