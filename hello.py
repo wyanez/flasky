7842188bb0128352ee44c9baf8ed6b02da70af6a
William Yanez <wyanez@gmail.com>
2014-10-15 01:56:43 -0430

    Add database support to view function index, search and create of user

--- hello.py
+++ hello.py
@@ -34,7 +34,7 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
@@ -53,17 +53,21 @@ class User(db.Model):
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
-        session['name'] = form.name.data
+        user = User.query.filter_by(username = form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] =form.name.data
         form.name.data = ''
         return redirect(url_for('index')) 
     return render_template('index.html',
-                            form=form, name=session.get('name'))
+                            form=form, name=session.get('name'),
+                            known = session.get('known',False))
 
 @app.route('/user/<name>')
 def user(name):
